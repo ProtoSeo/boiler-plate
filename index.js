9109a8fb64e1555602943cc50d3746b2d0dfef10
SeoSeunghun <proto_seo@naver.com>
2021-01-08 19:45:28 +0900

    Auth 기능 구현

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const {auth }  =require('./middleware/auth');
 //application/x-www-form-urlencoded 이렇게 된 데이터를 가져오기 위함
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -22,7 +23,7 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.post('/register',(req,res)=>{
+app.post('/api/users/register',(req,res)=>{
   //  회원가입할때 필요한 정보를 클라이언트로 받아오면 그 것들을 database에 넣어준다.
   const user = new User(req.body);
 
@@ -34,7 +35,7 @@ app.post('/register',(req,res)=>{
   })
 });
 
-app.post('/login',(req,res)=>{
+app.post('/api/users/login',(req,res)=>{
   //  요청된 이메일이 있는지 찾는다.
   User.findOne({email: req.body.email},(err,user)=>{
     if(!user){
@@ -66,9 +67,21 @@ app.post('/login',(req,res)=>{
       })
     })
   })
-  
+});
 
-  
-})
+app.get('/api/users/auth',auth,(req,res)=>{
+
+  //여기까지 middleware를 통과했다는 것은 인증이 통과되었다는 것
+  res.status(200).json({
+    _id:req.user._id,
+    isAdmin:req.user.role === 0 ? false:true,
+    isAuth:true,
+    email:req.user.email,
+    name:req.user.name,
+    lastname:req.user.lastname,
+    role:req.user.role,
+    image:req.user.image
+  });
+});
 
 app.listen(3000)
