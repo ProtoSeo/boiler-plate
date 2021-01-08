867c4ca71aa7ace0953759dedba4255b7eb1874d
SeoSeunghun <proto_seo@naver.com>
2021-01-08 14:15:44 +0900

    Mongo DB 연결

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express')
+const mongoose = require('mongoose');
+
 var app = express()
 
+mongoose.connect('mongodb+srv://ProtoSeo:<password>@boiler-plate.r7mki.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:true}
+)
+.then(()=>console.log('Mongo DB Connected...'))
+.catch(err=> console.log(err));
 app.get('/', function (req, res) {
   res.send('hello world')
 })
