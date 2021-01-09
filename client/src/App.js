17c060d259c632a10dc2d7352d426da2ec0a43e6
SeoSeunghun <proto_seo@naver.com>
2021-01-09 12:19:09 +0900

    reac-router-dom설치 및 App.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login" component={LoginPage}/>
+
+          <Route path="/register"component={RegisterPage}/>
+
+          <Route path="/"component={LandingPage}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
